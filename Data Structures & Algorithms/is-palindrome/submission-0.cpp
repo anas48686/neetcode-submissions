665b8110a2dbf6be27494877773a63016cf219b4
Mohammad Anas Hannan <mohammadanashannan@gmail.com>
2026-09-13 17:00:29 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l <= r) {
+            if(!isalnum(s[l])) 
+                l++;
+            if(!isalnum(s[r])) 
+                r--;
+            else {
+                char left = tolower(s[l]);
+                char right = tolower(s[r]);
+                if(left != right) return false;
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+};
